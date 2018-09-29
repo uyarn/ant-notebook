17a882f1047347fb41fe4b8bb69cbbce9ca1ce7d
yarnchen <yarnchenyuyang@gmail.com>
2018-09-30 00:33:26 +0800

    calendar style

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '',
+    motto: '每天起床第一句, 先给自己打个气',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -16,17 +16,18 @@ Page({
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../todoList/todoList'
-    })
+    let date = wx.getStorageSync('date')||''
+    if(date!=Date.getDate()){}
+    let encourage = wx.getStorageSync('encourage')||''
+
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
       let userInfo = app.globalData.userInfo
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-        motto: '你好,' + userInfo.nickName+'。需要帮你记点什么?'
+        hasUserInfo: true
+       
       })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -34,8 +35,7 @@ Page({
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true,
-          motto: '你好,' + res.userInfo.nickName + '。需要帮你记点什么?'
+          hasUserInfo: true
         })
       }
     } 
