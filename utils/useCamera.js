605bd8c1ba9fb3cb8d9378196ed9f7c84d976ec8
yarnchen <yarnchenyuyang@gmail.com>
2018-10-17 22:42:40 +0800

    fix image upload bugs

--- utils/useCamera.js
+++ utils/useCamera.js
@@ -6,17 +6,19 @@ const useCamera = (_this) =>{
     success:function (res) {
       // tempFilePath可以作为img标签的src属性显示图片
       let tempFilePaths = res.tempFilePaths
-      wx.request({
-        url: tempFilePaths[0],
-        method: 'GET',
-        responseType: 'arraybuffer',
-        success: function (res) {
-        let base64 = wx.arrayBufferToBase64(res.data);
-        _this.setData({
-          image: 'data:image/jpg;base64,' + base64
-          })
-        }
-      })   
+      _this.setData({ image: tempFilePaths[0]})
+      // wx.request({
+      //   url: tempFilePaths[0],
+      //   method: 'GET',
+      //   responseType: 'arraybuffer',
+      //   success: function (res) {
+      //   console.log(res.data)
+      //   let base64 = wx.arrayBufferToBase64(res.data);
+      //   _this.setData({
+      //     image: 'data:image/jpg;base64,' + base64
+      //     })
+      //   }
+      // })   
     }
   })
 }
